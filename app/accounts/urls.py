5fc38fe5fdb1c4cefa80ed9caa9568a1df96178a
vladislav <bincha.1997@gmail.com>
2020-01-07 22:50:50 +0900

    Added accounts view

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from accounts.views import SignUpView, LogInView, LogOutView
 
 
 app_name = 'accounts'
 
 urlpatterns = [
-
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LogInView.as_view(), name='login'),
+    path('logout/', LogOutView.as_view(), name='logout')
 ]
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
+
+class SignUpView(TemplateView):
+
+    template_name = 'accounts/signup.html'
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreationForm()
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+
+
+class LogInView(TemplateView):
+
+    template_name = 'accounts/login.html'
+
+    def get(self, request, *args, **kwargs):
+        form = AuthenticationForm()
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(
+                request,
+                username=username,
+                password=password
+                )
+            login(request, user)
+            return redirect('sender:index')
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+
+
+class LogOutView(TemplateView):
+
+    def get(self, request):
+        logout(request)
+        return redirect('accounts:login')
