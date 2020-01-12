1d2e9ea58f29595490dee53402707ce853fa00f2
vladislav <bincha.1997@gmail.com>
2020-01-12 22:12:38 +0900

    Added models

--- app/quiz/models.py
+++ app/quiz/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Quiz(models.Model):
+
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    success_text = models.TextField(blank=True)
+    fail_text = models.TextField(blank=True)
+    pass_mark = models.SmallIntegerField(
+        validators=[
+        MinValueValidator(0),
+        MaxValueValidator(100)
+        ])
+    
+    class Meta:
+
+        verbose_name = 'Quiz'
+        verbose_name_plural = 'Quizzes'
+
+    def __str__(self):
+        return f'Quiz: {self.title}'
+
+
+class Question(models.Model):
+
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    text = models.TextField()
+    success_mark = models.BooleanField(default=False)
+
+    class Meta:
+
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+
+    def __str__(self):
+        return f'Question: {self.text}'
