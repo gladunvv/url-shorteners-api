d237fdae548d02dfab840f24ae101818fe43c67b
vladislav <bincha.1997@gmail.com>
2020-01-25 22:32:58 +0900

    Added detail quiz view

--- app/quiz/urls.py
+++ app/quiz/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from quiz.views import QuizzesListView, QuestionsListView, QuestionView, IndexView
+from quiz.views import QuizzesListView, QuizDetailView, QuestionView, IndexView
 
 
 app_name = 'quiz'
@@ -7,6 +7,6 @@ app_name = 'quiz'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('list/', QuizzesListView.as_view(), name='quizzes_list'),
-    path('<str:title>', QuestionsListView.as_view(), name='questions_list'),
+    path('<str:title>', QuizDetailView.as_view(), name='quiz_detail'),
     path('<str:title>/questions', QuestionView.as_view(), name='question')
 ]
--- app/quiz/views.py
+++ app/quiz/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.views.generic import TemplateView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from quiz.models import Quiz, Question, Answer, UserAnswer
 
 
@@ -14,58 +14,21 @@ class QuizzesListView(TemplateView):
     template_name = 'quiz/quiz_view.html'
 
     def get(self, request, *args, **qwargs):
-        quiz = Quiz.objects.all()
+        quizzes = Quiz.objects.all()
+        context = {
+            'quizzes': quizzes
+        }
+        return render(request, self.template_name, context)
+
+
+class QuizDetailView(TemplateView):
+
+    template_name = 'quiz/quiz_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        quiz = get_object_or_404(Quiz, title=kwargs['title'])
         context = {
             'quiz': quiz
         }
         return render(request, self.template_name, context)
-    
 
-class QuestionsListView(TemplateView):
-
-    template_name = 'quiz/questions_list_view.html'
-
-    def get(self, request, *args, **kwargs):
-        quiz = Quiz.objects.get(title=kwargs['title'])
-        questions = quiz.questions.all()
-        context = {
-
-            'quiz': quiz,
-            'questions': questions
-        }
-        return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        quiz = Quiz.objects.get(title=kwargs['title'])
-        quiz.user = request.user
-        quiz.save()
-        return redirect('quiz:question', quiz.title)
-
-
-class QuestionView(TemplateView):
-
-    template_name = 'quiz/question_view.html'
-
-    def get(self, request, *args, **kwargs):
-        quiz = Quiz.objects.get(title=kwargs['title'])
-        question = Question.objects.filter(quiz=quiz)
-        paginator = Paginator(question, 1)
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-        context = {
-            'page_obj': page_obj,
-        }
-        return render(request, self.template_name, context)
-
-
-    def post(self, request, *args, **kwargs):
-        
-        answer = Answer.objects.get(variant=request.POST.get('answer'))
-        user_answer = UserAnswer.objects.create(
-            user = request.user,
-            question = answer.question,
-            answer = answer
-        )
-        user_answer.save()
-
-        return redirect('quiz:question',title=answer.question.quiz.title)
