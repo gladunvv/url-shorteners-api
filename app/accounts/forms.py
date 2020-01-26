4770dda87b4ecb2454d2f1d5afb18952842b025c
vladislav <bincha.1997@gmail.com>
2020-01-26 23:16:15 +0900

    Update models accounts

--- app/accounts/forms.py
+++ app/accounts/forms.py
@@ -1,9 +1,27 @@
 from django.contrib.auth.forms import UserCreationForm
 from accounts.models import User
 
-class UserCreationFormCustom(UserCreationForm):
+class StudentCreationForm(UserCreationForm):
     
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = User
-    
-        fields = ('username', 'password1', 'password2', 'role')
+
+    def save(self):
+        user = super().save(commit=False)
+        user.is_student = True
+        user.save()
+        student = Student.objects.create(user=user)
+        return user
+
+
+class TeacherCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def save(self):
+        user = super().save(commit=False)
+        user.is_teacher = True
+        user.save()
+        teacher = Teacher.objects.create(user=user)
+        return user
--- app/accounts/models.py
+++ app/accounts/models.py
@@ -3,9 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 
-    USER_ROLE = (
-        ('student', 'Student'),
-        ('teacher', 'Teacher'),
-    )
-
-    role = models.CharField(choices=USER_ROLE, max_length=7, default='student')
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
