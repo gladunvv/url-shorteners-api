c413908348e8da971fb78039fdb542a9f6715ae9
vladislav <bincha.1997@gmail.com>
2020-01-29 23:24:52 +0900

    Update quiz admin

--- app/quiz/admin.py
+++ app/quiz/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from quiz.models import Quiz, Question, Answer, StudentAnswer
+from quiz.models import Quiz, Question, Answer, StudentAnswer, Teacher, TakenQuiz, Student
 
 
 @admin.register(Quiz)
@@ -19,3 +19,18 @@ class EmailAdmin(admin.ModelAdmin):
 @admin.register(StudentAnswer)
 class StudentAnswerAdmin(admin.ModelAdmin):
     list_display = ('user', 'answer')
+
+
+@admin.register(Teacher)
+class EmailAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Student)
+class EmailAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TakenQuiz)
+class StudentAnswerAdmin(admin.ModelAdmin):
+    pass
